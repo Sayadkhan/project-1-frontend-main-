372dbe0bf5e468778f59c2f86099c2054c747f37
Abdullah Al Sayad <abdullahalsayad03@gmail.com>
2025-01-08 02:55:50 +0600

    url changed

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api", // Replace with your backend URL
+  // baseURL: "http://localhost:8000/api",
+  baseURL: "https://project-1-bsckend.onrender.com/api", // Replace with your backend URL
   withCredentials: true, // Allow cookies if needed (JWT in cookies)
 });
 
